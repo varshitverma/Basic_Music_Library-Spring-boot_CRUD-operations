3d42ff23a00d73f27abb9c780a840ff7fcbda62f
Varshit Verma <pvarshitv@gmail.com>
2023-02-25 18:51:46 +0530

    Update SongController.java

--- src/main/java/com/example/song/SongController.java
+++ src/main/java/com/example/song/SongController.java
@@ -1,38 +1,28 @@
-/*
- * You can use the following import statements
- * 
- * import org.springframework.web.bind.annotation.*;
- * import java.util.*;
- * 
- */
-
-// Write your code here
-
 package com.example.song;
 
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
-import com.example.song.Song;
 import com.example.song.SongService;
+import com.example.song.Song;
 
-@RestController 
+@RestController
 public class SongController {
     SongService songService = new SongService();
 
     @GetMapping("/songs")
-    public ArrayList<Song> getSongs() {
-        return songService.getSongs();
+    public ArrayList<Song> getAllSongs() {
+        return songService.getAllSongs();
+    }
+
+    @GetMapping("/songs/{songId}")
+    public Song getSongById(@PathVariable("songId") int songId) {
+        return songService.getSongById(songId);
     }
 
     @PostMapping("/songs")
     public Song addSong(@RequestBody Song song) {
         return songService.addSong(song);
-    }
-    
-    @GetMapping("/songs/{songId}")
-    public Song getSongById(@PathVariable("songId") int songId) {
-        return songService.getSongById(songId);
     }
 
     @PutMapping("/songs/{songId}")
