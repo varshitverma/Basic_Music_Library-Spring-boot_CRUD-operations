9e85d8a4b25e069b9397a6960fc8a48b6b282b35
Varshit Verma <pvarshitv@gmail.com>
2023-02-25 19:00:10 +0530

    Update SongRepository.java

--- src/main/java/com/example/song/SongRepository.java
+++ src/main/java/com/example/song/SongRepository.java
@@ -1,12 +1,14 @@
 package com.example.song;
 
-import java.util.*;
+import com.example.song.Song;
 
-public interface SongRepository {
-    ArrayList<Song> getAllSongs();
+import java.util.ArrayList;
 
-    Song getSongById(int songId);
+public interface SongRepository{
+    ArrayList<Song> getSongs();
+    
     Song addSong(Song song);
+    Song getSongById(int songId);
     Song updateSong(int songId, Song song);
     void deleteSong(int songId);
 }
